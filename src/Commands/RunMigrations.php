d8e6a33ccbcfa6cae3bf47f32061158c4e7c8d91
Ryan Solida <ryansolida@gmail.com>
2021-08-17 15:52:13 -0400

    don't kill tables

--- src/Commands/RunMigrations.php
+++ src/Commands/RunMigrations.php
@@ -52,101 +52,104 @@ class RunMigrations extends Command
 
             $scaffold = new SilScaffold($data['slug']);
             Schema::dropIfExists($scaffold->table);
-            Schema::create($scaffold->table,function($table) use ($scaffold){
-                $table->increments('id');
-                foreach ($scaffold->fields as $field=>$field_data){
-                    
-                    $data_type = 'string';
-                    $field_name = $field;
+            if ( !Schema::hasTable($scaffold->table) ) {
 
-                    if ( substr($field,-3) == '_id' ){
-                        $data_type = 'biginteger';
-                    }
+                Schema::create($scaffold->table,function($table) use ($scaffold){
+                    $table->increments('id');
+                    foreach ($scaffold->fields as $field=>$field_data){
+                        
+                        $data_type = 'string';
+                        $field_name = $field;
 
-                    if ( @$field_data['type'] == 'datetime' ){
-                        $data_type = 'datetime';
-                    }
-
-                    if ( is_array($field_data) ){
-                        $text_field_types = [
-                            'image','repeater','textarea','wysiwyg'
-                        ];
-                        if ( in_array($field_data['type'],$text_field_types) ){
-                            $data_type = 'text';
+                        if ( substr($field,-3) == '_id' ){
+                            $data_type = 'biginteger';
                         }
-                    }
 
-                    if ( @$scaffold->relationships && array_key_exists($field_name,$scaffold->relationships) ){
-                        $relationship = new SilScaffoldRelationship($scaffold->relationships[$field_name]);
-                        if ( !$relationship->needs_field ){
+                        if ( @$field_data['type'] == 'datetime' ){
+                            $data_type = 'datetime';
+                        }
+
+                        if ( is_array($field_data) ){
+                            $text_field_types = [
+                                'image','repeater','textarea','wysiwyg'
+                            ];
+                            if ( in_array($field_data['type'],$text_field_types) ){
+                                $data_type = 'text';
+                            }
+                        }
+
+                        if ( @$scaffold->relationships && array_key_exists($field_name,$scaffold->relationships) ){
+                            $relationship = new SilScaffoldRelationship($scaffold->relationships[$field_name]);
+                            if ( !$relationship->needs_field ){
+                                continue;
+                            } else {
+                                $data_type = 'integer';
+                                $field_name = $field_name.'_id';
+                            }
+                        }
+
+                        $table->$data_type($field_name)->nullable();
+                    }
+                    $table->timestamps();
+                });
+
+                //build many to many tables
+                if ( !@$data['relationships'] ){
+                    $data['relationships'] = [];
+                }
+
+                $many_to_many_tables_made = [];
+                
+                foreach ($data['relationships'] as $r_name=>$r){
+                    if ( $r[0] == 'belongsToMany' ){
+                        
+                        $scaffold_item = $config[$r_name];
+                        $inverse_table = $scaffold_item['table'];
+                    
+                        $tables = [\Str::singular($inverse_table),\Str::singular($data['table'])];
+                        natsort($tables);
+                        $table_name = implode('_',$tables);
+                        if ( in_array($table_name,$many_to_many_tables_made) ){
                             continue;
-                        } else {
-                            $data_type = 'integer';
-                            $field_name = $field_name.'_id';
                         }
+                        Schema::dropIfExists($table_name);
+                        $fields = array_map(function($table){
+                            return $table.'_id';
+                        },$tables);
+                        Schema::create($table_name,function($table) use ($fields){
+                            foreach ($fields as $f){
+                                $table->integer($f);
+                            }
+                        });
+                        $many_to_many_tables_made[] = $table_name;
                     }
 
-                    $table->$data_type($field_name)->nullable();
                 }
-                $table->timestamps();
-            });
 
-            //build many to many tables
-            if ( !@$data['relationships'] ){
-                $data['relationships'] = [];
-            }
+                //add dummy data
+                if ( @$data['stub_data'] ){
+                    if ( is_callable($data['stub_data'],TRUE,$callable_name) ){
+                        $data['stub_data'] = $callable_name();
+                    }
 
-            $many_to_many_tables_made = [];
-            
-            foreach ($data['relationships'] as $r_name=>$r){
-                if ( $r[0] == 'belongsToMany' ){
+                    foreach ($data['stub_data'] as $entry){
+                        $values = [];
+                        foreach ($entry as $k=>$v){
+                            if ( stripos($v,'()') !== FALSE ){ //run this function 
+                                eval('$v = '.$v.';');
+                            }
+
+                            $values[$k] = $v;
+                        }
+                        $values['created_at'] = date("Y-m-d H:i:s");
+                        $values['updated_at'] = date("Y-m-d H:i:s");
+                        \DB::table($data['table'])->insert($values);
+                    }
+
                     
-                    $scaffold_item = $config[$r_name];
-                    $inverse_table = $scaffold_item['table'];
-                
-                    $tables = [\Str::singular($inverse_table),\Str::singular($data['table'])];
-                    natsort($tables);
-                    $table_name = implode('_',$tables);
-                    if ( in_array($table_name,$many_to_many_tables_made) ){
-                        continue;
-                    }
-                    Schema::dropIfExists($table_name);
-                    $fields = array_map(function($table){
-                        return $table.'_id';
-                    },$tables);
-                    Schema::create($table_name,function($table) use ($fields){
-                        foreach ($fields as $f){
-                            $table->integer($f);
-                        }
-                    });
-                    $many_to_many_tables_made[] = $table_name;
                 }
-
             }
-
-            //add dummy data
-            if ( @$data['stub_data'] ){
-                if ( is_callable($data['stub_data'],TRUE,$callable_name) ){
-                    $data['stub_data'] = $callable_name();
-                }
-
-                foreach ($data['stub_data'] as $entry){
-                    $values = [];
-                    foreach ($entry as $k=>$v){
-                        if ( stripos($v,'()') !== FALSE ){ //run this function 
-                            eval('$v = '.$v.';');
-                        }
-
-                        $values[$k] = $v;
-                    }
-                    $values['created_at'] = date("Y-m-d H:i:s");
-                    $values['updated_at'] = date("Y-m-d H:i:s");
-                    \DB::table($data['table'])->insert($values);
-                }
-
-                
-            }
-            
+        
 
 
         }
